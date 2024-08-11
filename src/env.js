55925610901a0c423a7eaee1797a6e6c5c472d88
danielvaswani2 <daniel.vaswani@jumbo.com>
2024-08-11 19:37:35 +0700

    try api route

--- src/env.js
+++ src/env.js
@@ -26,7 +26,7 @@ export const env = createEnv({
    */
   runtimeEnv: {
     NODE_ENV: process.env.NODE_ENV,
-    SERVICE_ACCOUNT: z.unknown(),
+    SERVICE_ACCOUNT: process.env.SERVICE_ACCOUNT,
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
   /**
