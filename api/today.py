a96724037a81ba984423f866dcc63d0151fad1fa
danielvaswani2 <daniel.vaswani@jumbo.com>
2024-08-12 13:32:08 +0700

    try cors yet again

--- api/today.py
+++ api/today.py
@@ -20,7 +20,7 @@ class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.send_header('Content-type','text/plain')
-        self.send_header('Access-Control-Allow-Credentials', true)
+        self.send_header('Access-Control-Allow-Credentials', True)
         self.send_header('Access-Control-Allow-Origin', '*')
         self.send_header('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
         self.send_header(
